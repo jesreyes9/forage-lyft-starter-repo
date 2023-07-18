cc5215401b1075f152b3c91ffbaaed9bea3f9ba1
jesreyes9 <reyesjesel9@gmail.com>
2023-07-18 00:13:43 -0500

    Modifying needs service functions on nubbin and spindler battery classes

--- engine/battery/nubbin_battery.py
+++ engine/battery/nubbin_battery.py
@@ -1,5 +1,16 @@
+from abc import ABC
+from datetime import datetime, timedelta
 from battery import Battery
 
-class NubbinBattery(Battery):
+
+class NubbinBattery(Battery, ABC):
+    def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = current_date
+
     def needs_service(self):
-        pass
+        # every 4 years
+        if self.current_date - self.last_service_date > timedelta(days=1460):
+            return True
+        else:
+            return False
--- engine/battery/spindler_battery.py
+++ engine/battery/spindler_battery.py
@@ -1,5 +1,17 @@
+from abc import ABC
+from datetime import timedelta
+
 from battery import Battery
 
-class SpindlerBattery(Battery):
+
+class SpindlerBattery(Battery, ABC):
+    def __init__(self, last_service_date, current_date):
+        super().__init__(last_service_date)
+        self.current_date = current_date
+
     def needs_service(self):
-        pass
+        # every 2 years
+        if self.current_date - self.last_service_date > timedelta(days=730):
+            return True
+        else:
+            return False
